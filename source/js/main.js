eb58ebf2d77cd14883abd9d820e29ede99e32743
Evgeniy Kolegov <jpriest1987@gmail.com>
2020-05-19 21:04:24 +0700

    Множественные правки

--- source/js/main.js
+++ source/js/main.js
@@ -227,3 +227,60 @@
     resideLogo();
   });
 })();
+
+// input mask validation
+
+(function () {
+  var KeyCode = {
+    BACKSPACE: 8,
+    ESCAPE: 27,
+    ENTER: 13
+  };
+
+  function setInputMask(section) {
+    var cbForm = section.querySelector('form');
+    var telInput = cbForm.querySelector('input[type="tel"]');
+    var swap;
+
+
+    telInput.addEventListener('focus', function () {
+      telInput.value = swap || '+7(';
+    });
+
+    telInput.addEventListener('input', function () {
+      if (telInput.value.match(/\+$|\+7$|\+7\([^\d]/)) {
+        telInput.value = '+7('; // не допускает значений '+', '+7' и ввода после скобки нечисловых значений
+      }
+      if (!telInput.value.match(/\)/)) { // проверка на наличие закрывающей скобки
+        if (telInput.value.match(/\+7\(\d+/)) {
+          telInput.value = telInput.value.match(/\+7\(\d+/); // записывает в значение поля вводимые числа и блокирует ввод нечисловых значений
+        }
+
+        if (telInput.value.match(/\+7\(\d{3}/)) { // добавляет скобку после ввода 3 чисел
+          telInput.value += ')';
+        }
+      } else {
+        telInput.value = telInput.value.match(/\+7\(\d{3}\)\d{0,7}/);
+      }
+
+      telInput.addEventListener('keydown', function (evt) {
+        if (evt.keyCode === KeyCode.BACKSPACE && telInput.value.match(/\+7\(\d{3}\)$/)) { // добавляет возможность удалять закрывающую скобку и число перед ней
+          telInput.value = telInput.value.slice(0, -1);
+        }
+      });
+
+      telInput.addEventListener('change', function () {
+        swap = telInput.value;
+      });
+    });
+
+    telInput.addEventListener('invalid', function () {
+      telInput.setCustomValidity('Значение поля должно быть в формате: +7(999)9999999');
+    });
+
+  }
+
+  var formSection = document.querySelector('.form');
+
+  setInputMask(formSection);
+})();
